376daf398cc617efb1ec8a1584921c4b79538841
Yugandhar786 <vgyugandhargopi2708@gmail.com>
2024-07-28 01:35:59 +0530

    played with Methods

--- src/sample/Arithmatic.java
+++ src/sample/Arithmatic.java
@@ -1,18 +1,29 @@
 package sample;
-
+import java.util.Scanner;
+import loginvalidating.*;
 public class Arithmatic {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=10,b=20,c=30;
-		int d,e,f;
-		d=a+b;
-		e=c*a;
-		f=b/a;
-		System.out.println("addition of a and b "+d);
-		System.out.println("multiplication of c and a " +e);
-		System.out.println("Division of b and b "+f);
+		int a=259259,b=259259,c=259259;
+		int d;
+		d=a+b+c;
+		System.out.println("\n\n\n\nYou Secret Key is ="+d);
+		keygen();
+		
 
 	}
+	public static void keygen() {
+		Scanner key1 = new Scanner(System.in);  
+	    System.out.println("\n\n\n\nEnter Secret Key:");
+
+	    String secretKey = key1.nextLine(); 
+	    
+	    if (secretKey.equals("777777"))
+	    {
+	    	Message.main(null);
+	    }
+	}
+	
 
 }
--- src/sample/Loops.java
+++ src/sample/Loops.java
@@ -1,9 +1,14 @@
 package sample;
 import java.util.Scanner;
+import loginvalidating.*;
+
 public class Loops{
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+	
 		
 		Scanner obj1 = new Scanner(System.in);  
 	    System.out.println("Enter username");
@@ -14,24 +19,13 @@ public class Loops{
 	    Scanner obj2 = new Scanner(System.in);  
 	    System.out.println("Enter Password");
 	    String passWord = obj2.nextLine(); 
-	    System.out.println(userName);
-	    System.out.println(passWord);
-	    
-	    if(userName.equals("admin") && passWord.equals("Yuga"))
-	    {
-	    	System.out.println("Login Sucessfull");
-	    	System.out.println("Welcome");
-	    	
-	    	
-	    }
-	    else
-	    {
-	    	System.out.println("Login Failed");
-	    }
-	
-		
-		
+//	    System.out.println(userName);
+//	    System.out.println(passWord);
+	    LoginValidate.pushcred(userName, passWord);
+	   
 
 	}
 
 }
+
+
